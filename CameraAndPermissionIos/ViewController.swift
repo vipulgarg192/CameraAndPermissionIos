482766a1f479adb4dad2c704bcd11573b9ce7451
vipulgarg192 <Vipulgarg192@gmail.com>
2019-07-15 18:14:10 -0400

    Initial Commit

---
+++ CameraAndPermissionIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraAndPermissionIos
+//
+//  Created by MacStudent on 2019-07-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
